22a0d0415f0a9409bb82165807cd459f8bdf55a7
ikaika <willtolivehaver@gmail.com>
2022-10-01 09:56:07 -0500

    new app.tsx

--- moogle/src/components/smallCard.tsx
+++ moogle/src/components/smallCard.tsx
@@ -1,40 +1,45 @@
 import React from "react";
 import { Card, Container, Grid, Avatar, Typography, Box } from "@mui/material";
+import TestImage from "../assets/test.jpeg";
 
 const smallCard = () => {
 	return (
-		<Container>
-			<Card>
-				<Grid
-					container
-					direction='column'
-					justifyContent='center'
-					alignItems='center'
-				>
-					<Grid item>
+		<Container sx={{ width: "200px", height: "100px" }}>
+			<Grid
+				container
+				direction='column'
+				justifyContent='center'
+				alignItems='center'
+				sx={{ backgroundColor: "white", borderRadius: 1 }}
+			>
+				<Grid item sx={{ width: "80%" }}>
+					<Grid
+						container
+						direction='row'
+						justifyContent='space-between'
+						alignItems='center'
+					>
 						<Grid
-							container
-							direction='row'
-							justifyContent='space-between'
-							alignItems='center'
+							item
+							sx={{ marginTop: "-50px", height: "50px", width: "50px" }}
 						>
-							<Grid item>
-								<Card>
-									<Avatar></Avatar>
-								</Card>
-							</Grid>
-							<Grid item>
-								<Typography></Typography>
-								<Typography></Typography>
-							</Grid>
+							<Card sx={{ height: "100%", width: "100%" }}>
+								<Avatar sx={{ height: "100%", width: "100%" }}></Avatar>
+							</Card>
+						</Grid>
+						<Grid item>
+							<Typography>DEEZ</Typography>
+							<Typography>DEEZ</Typography>
 						</Grid>
 					</Grid>
-					<Grid item>
-						<Box></Box>
-						<Typography></Typography>
-					</Grid>
 				</Grid>
-			</Card>
+				<Grid item sx={{ justifyContent: "flex-start" }}>
+					<Box>
+						<Avatar></Avatar>
+					</Box>
+					<Typography>DEEZ</Typography>
+				</Grid>
+			</Grid>
 		</Container>
 	);
 };
--- moogle/src/pages/home.tsx
+++ moogle/src/pages/home.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Container, Box, Typography, Grid } from "@mui/material";
+import SmallCard from "../components/smallCard";
 
 const home = () => {
 	return (
-		<Container>
+		<Container sx={{}}>
 			<Grid
 				container
 				direction='column'
 				alignItems='center'
 				justifyContent='center'
 			>
-				<Grid item>
-					<Box></Box>
+				<Grid
+					item
+					sx={{
+						height: "1000px",
+						width: "1000px",
+						backgroundColor: "gray",
+						paddingTop: "50px",
+					}}
+				>
+					<SmallCard />
 				</Grid>
 				<Grid item>
 					<Box></Box>
