e4bed82ed9b2978519dde082afd8a379613e02dc
ikaika <willtolivehaver@gmail.com>
2022-10-01 11:02:07 -0500

    material ui icons

--- moogle/src/components/smallCard.tsx
+++ moogle/src/components/smallCard.tsx
@@ -1,43 +1,86 @@
 import React from "react";
-import { Card, Container, Grid, Avatar, Typography, Box } from "@mui/material";
-import TestImage from "../assets/test.jpeg";
+import {
+	Card,
+	Container,
+	Grid,
+	Avatar,
+	Typography,
+	Box,
+	Divider,
+} from "@mui/material";
 
-const smallCard = () => {
+type Props = {
+	icon: string;
+	purpose: string;
+	content: string;
+	smallIcon: string;
+	details: string;
+};
+
+const smallCard = (props: Props) => {
+	const { icon, purpose, content, smallIcon, details } = props;
+
 	return (
-		<Container sx={{ width: "200px", height: "100px" }}>
+		<Container
+			sx={{ minHeight: "100px", minWidth: "200px" }}
+			disableGutters={true}
+		>
 			<Grid
 				container
 				direction='column'
 				justifyContent='center'
 				alignItems='center'
-				sx={{ backgroundColor: "white", borderRadius: 1 }}
+				sx={{
+					backgroundColor: "white",
+					borderRadius: 1,
+					height: "100%",
+				}}
 			>
-				<Grid item sx={{ width: "80%" }}>
+				<Grid item sx={{ height: "50%", width: "100%" }}>
 					<Grid
 						container
 						direction='row'
 						justifyContent='space-between'
 						alignItems='center'
+						sx={{ width: "90%", margin: 0 }}
 					>
 						<Grid
 							item
-							sx={{ marginTop: "-50px", height: "50px", width: "50px" }}
+							sx={{ marginTop: "-50px", height: "70px", width: "70px" }}
 						>
 							<Card sx={{ height: "100%", width: "100%" }}>
-								<Avatar sx={{ height: "100%", width: "100%" }}></Avatar>
+								<Avatar
+									sx={{ height: "100%", width: "100%" }}
+									src={icon}
+									alt='icon'
+								/>
 							</Card>
 						</Grid>
-						<Grid item>
-							<Typography>DEEZ</Typography>
-							<Typography>DEEZ</Typography>
+						<Grid item sx={{ margin: "10px" }}>
+							<Typography sx={{ fontSize: 20 }}>{purpose}</Typography>
+							<Typography sx={{ fontSize: 25 }}>{content}</Typography>
+						</Grid>
+						<Grid item sx={{ height: "10px", width: "100%" }}>
+							<Divider variant='fullWidth' />
 						</Grid>
 					</Grid>
 				</Grid>
-				<Grid item sx={{ justifyContent: "flex-start" }}>
-					<Box>
-						<Avatar></Avatar>
-					</Box>
-					<Typography>DEEZ</Typography>
+				<Grid item sx={{ width: "100%", margin: "10px" }}>
+					<Grid
+						container
+						direction='row'
+						justifyContent='flex-start'
+						alignItems='center'
+					>
+						<Grid item>
+							<Box>
+								<Avatar src={smallIcon} alt='small icon' />
+							</Box>
+						</Grid>
+						<Grid item>
+							<Typography>{details}</Typography>
+						</Grid>
+					</Grid>
 				</Grid>
 			</Grid>
 		</Container>
--- moogle/src/pages/home.tsx
+++ moogle/src/pages/home.tsx
@@ -1,26 +1,35 @@
 import React from "react";
 import { Container, Box, Typography, Grid } from "@mui/material";
 import SmallCard from "../components/smallCard";
+import TestImage from "../assets/test.jpeg";
+import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
 
 const home = () => {
 	return (
-		<Container sx={{}}>
+		<Container
+			sx={{ width: "100%", height: "100%", backgroundColor: "gray" }}
+			disableGutters={true}
+		>
 			<Grid
 				container
-				direction='column'
+				direction='row'
 				alignItems='center'
 				justifyContent='center'
+				sx={{
+					height: "500px",
+					width: "300px",
+					paddingTop: "50px",
+					backgroundColor: "red",
+				}}
 			>
-				<Grid
-					item
-					sx={{
-						height: "1000px",
-						width: "1000px",
-						backgroundColor: "gray",
-						paddingTop: "50px",
-					}}
-				>
-					<SmallCard />
+				<Grid item sx={{}}>
+					<SmallCard
+						purpose='test'
+						content='testing'
+						details='testtest'
+						icon={AcUnitIcon}
+						smallIcon={AcUnitIcon}
+					/>
 				</Grid>
 				<Grid item>
 					<Box></Box>
